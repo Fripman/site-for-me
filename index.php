9631523c5165a9deb23f8e01447ac263f0b45d41
Fripman <valentin.richard30@gmail.com>
2023-03-07 09:56:17 +0100

    stream ok

--- index.php
+++ index.php
@@ -12,7 +12,19 @@
       include('header.php');
       ?>
       <h1>Stream</h1>
-      <p>Regardez les derniers streams en direct</p>
+      <!-- Add a placeholder for the Twitch embed -->
+      <div id="twitch-embed"></div>
+
+      <!-- Load the Twitch embed script -->
+      <script src="https://player.twitch.tv/js/embed/v1.js"></script>
+
+      <!-- Create a Twitch.Player object. This will render within the placeholder div -->
+      <script type="text/javascript">
+            new Twitch.Player("twitch-embed", {
+                  channel: "fripman"
+            });
+      </script>
+      
       <?php
       include('footer.php');
       ?>
