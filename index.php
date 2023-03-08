40f684cadb99fdff6a1a57d1dfbffb5449d2e385
Fripman <valentin.richard30@gmail.com>
2023-03-08 21:36:55 +0100

    Update index.php

--- index.php
+++ index.php
@@ -17,9 +17,9 @@
 
     <div class="stream">
 
-        <iframe src="https://player.twitch.tv/?channel=fripman&parent=fripman.fripman.fr" frameborder="0" allowfullscreen="true" scrolling="no" height="800" width="80%"></iframe>
+        <iframe src="https://player.twitch.tv/?channel=fripman&parent=www.fripman.fr" frameborder="0" allowfullscreen="true" scrolling="no" height="800" width="80%"></iframe>
 
-        <iframe id="chat_embed" src="https://www.twitch.tv/embed/fripman/chat?parent=fripman.fripman.fr" height="800" width="20%"></iframe>
+        <iframe id="chat_embed" src="https://www.twitch.tv/embed/fripman/chat?parent=www.fripman.fr" height="800" width="20%"></iframe>
     </div>
 
     <?php
