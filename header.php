82f2ebd9d6a5606e1852abcbf5af62d53edaa23d
Fripman <valentin.richard30@gmail.com>
2023-03-08 22:40:47 +0100

    z

--- header.php
+++ header.php
@@ -1,12 +1,11 @@
 <header>
-    <ul>
-        <li><img src="loggo_tansparent.png" alt="logo_fripman" class="logo"></li>
-        <nav>
-
+    <nav>
+        <ul>
+            <li><img src="loggo_tansparent.png" alt="logo_fripman" class="logo"></li>
             <li><a href="index.php">Stream</a></li>
             <li><a href="video.php">videos</a></li>
             <li><a href="mon_parcours.php">Mon parcours</a></li>
+        </ul>
+    </nav>
 
-        </nav>
-    </ul>
 </header>
